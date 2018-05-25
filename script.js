83bc1c9e46876a099cf27c17227b62f0dccdb2d9
Peter Lada <peter.lada@better.com>
2018-05-25 11:33:21 -0400

    latest changes

--- script.js
+++ script.js
@@ -1,68 +1,9 @@
-function preventDefaults (e) {
-  e.preventDefault();
-  e.stopPropagation();
-}
-
-function handleDrop(e) {
-  let dt = e.dataTransfer;
-  let files = dt.files;
-
-  handleFiles(files);
-}
-
-function handleFiles(files) {
-  ([...files]).forEach(uploadAndPreviewFile);
-}
-
-function uploadAndPreviewFile(file) {
-  uploadFile(file);
-  previewFile(file);
-}
-
-function uploadFile(file) {
-  let url = 'http://peterlada.com';
-  let formData = new FormData();
-
-  formData.append('file', file);
-
-  fetch(url, {
-    method: 'POST',
-    body: formData
-  })
-  .then(() => { /* Done. Inform the user */ })
-  .catch(() => { /* Error. Inform the user */ });
-}
-
-function uploadFileLegacy(file) {
-  var url = 'YOUR URL HERE';
-  var xhr = new XMLHttpRequest();
-  var formData = new FormData();
-  xhr.open('POST', url, true);
-
-  xhr.addEventListener('readystatechange', e => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // Done. Inform the user
-    } else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Error. Inform the user
-    }
-  });
-
-  formData.append('file', file);
-  xhr.send(formData);
-}
-
-function previewFile(file) {
-  let reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onloadend = function() {
-    let img = document.createElement('img');
-    img.src = reader.result;
-    document.getElementById('gallery').appendChild(img);
-  }
-}
+let handleFiles;
 
 document.addEventListener("DOMContentLoaded", event => {
   let dropArea = document.getElementById('drop-area');
+  let uploadProgress = [];
+  let progressBar = document.getElementById('progress-bar');
 
   ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
     dropArea.addEventListener(eventName, preventDefaults, false);
@@ -76,6 +17,11 @@ document.addEventListener("DOMContentLoaded", event => {
     dropArea.addEventListener(eventName, unhighlight, false);
   })
 
+  function preventDefaults (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
   function highlight(e) {
     dropArea.classList.add('highlight');
   }
@@ -85,4 +31,69 @@ document.addEventListener("DOMContentLoaded", event => {
   }
 
   dropArea.addEventListener('drop', handleDrop, false);
+
+  function initializeProgress(numFiles) {
+    progressBar.value = 0;
+    uploadProgress = [];
+
+    for(let i = numFiles; i > 0; i--) {
+      uploadProgress.push(0)
+    }
+  }
+
+  function updateProgress(fileNumber, percent) {
+    uploadProgress[fileNumber] = percent
+    let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
+    progressBar.value = total
+  }
+
+  function handleDrop(e) {
+    let dt = e.dataTransfer;
+    let files = dt.files;
+
+    handleFiles(files);
+  }
+
+  global handleFiles = files => {
+    initializeProgress(files.length);
+    ([...files]).forEach(uploadAndPreviewFile);
+  }
+
+  function uploadAndPreviewFile(file) {
+    uploadFile(file);
+    previewFile(file);
+  }
+
+  function uploadFile(file, i) {
+    var url = 'http://peterlada.com';
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData();
+    xhr.open('POST', url, true);
+
+    xhr.upload.addEventListener("progress", function(e) {
+      updateProgress(i, (e.loaded * 100.0 / e.total) || 100);
+    })
+
+    xhr.addEventListener('readystatechange', e => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        // Done. Inform the user
+      } else if (xhr.readyState == 4 && xhr.status != 200) {
+        // Error. Inform the user
+      }
+    });
+
+    formData.append('file', file);
+    xhr.send(formData);
+  }
+
+  function previewFile(file) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function() {
+      let img = document.createElement('img');
+      img.src = reader.result;
+      document.getElementById('gallery').appendChild(img);
+    }
+  }
+
 });
